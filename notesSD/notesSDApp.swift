ecffa1a5ab35062ced24e2bc92d7b255163459ed
bhavukarora03 <Bhavuk.arora03@gmail.com>
2024-02-27 23:00:18 +0530

    Initial Commit

---
+++ notesSD/notesSDApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesSDApp.swift
+//  notesSD
+//
+//  Created by Bhavuk Arora on 27/02/24.
+//
+
+import SwiftUI
+
+@main
+struct notesSDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
